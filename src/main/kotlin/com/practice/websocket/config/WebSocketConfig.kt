4521803f472664f90fb1b088a6dbd2b6ec709752
juneunbae <dmsqo1403@naver.com>
2026-01-04 19:18:12 +0900

    Config 파일 추가
    WebSocketConfig.kt
    - 설정 파일 생성

---
+++ src/main/kotlin/com/practice/websocket/config/WebSocketConfig.kt
@@ -0,0 +1,24 @@
+package com.practice.websocket.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+// WebSocket + STOMP 기반 메시지 브로커 구조 사용
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+    override fun configureMessageBroker(registry: MessageBrokerRegistry?) {
+        // 클라이언트가 구독하는 prefix
+        registry?.enableSimpleBroker("/topic")
+
+        // 클라이언트가 서버로 보내는 prefix
+        registry?.setApplicationDestinationPrefixes("/app")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry?) {
+        registry?.addEndpoint("/ws")?.setAllowedOriginPatterns("*")
+    }
+}
